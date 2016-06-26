ea5618a382bd6d34fe07fa0d824522d5be2dc833
Alex-the-666 <alex.rowlands@cox.net>
2016-06-26 09:17:01 -0700

    Kapok doesn't generate?

--- src/main/java/com/github/alexthe666/archipelago/block/BlockArchipelagoSapling.java
+++ src/main/java/com/github/alexthe666/archipelago/block/BlockArchipelagoSapling.java
@@ -2,6 +2,7 @@ package com.github.alexthe666.archipelago.block;
 
 import com.github.alexthe666.archipelago.Archipelago;
 import com.github.alexthe666.archipelago.enums.EnumTrees;
+
 import net.minecraft.block.*;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
@@ -13,6 +14,7 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenMegaJungle;
 import net.minecraft.world.gen.feature.WorldGenShrub;
 import net.minecraft.world.gen.feature.WorldGenTrees;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -21,93 +23,99 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import java.util.Random;
 
 public class BlockArchipelagoSapling extends BlockBush implements IGrowable {
-    public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
-    protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
-    private EnumTrees treeType;
+	public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
+	protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
+	private EnumTrees treeType;
 
-    public BlockArchipelagoSapling(EnumTrees tree) {
-        this.setSoundType(SoundType.PLANT);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(STAGE, 0));
-        this.setCreativeTab(Archipelago.tab);
-        this.setUnlocalizedName("archipelago." + tree.name().toLowerCase() + "_sapling");
-        this.setCreativeTab(Archipelago.tab);
-        GameRegistry.registerBlock(this, tree.name().toLowerCase() + "_sapling");
-        Archipelago.PROXY.addItemRender(Item.getItemFromBlock(this), tree.name().toLowerCase() + "_sapling");
-        this.treeType = tree;
-    }
+	public BlockArchipelagoSapling(EnumTrees tree) {
+		this.setSoundType(SoundType.PLANT);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(STAGE, 0));
+		this.setCreativeTab(Archipelago.tab);
+		this.setUnlocalizedName("archipelago." + tree.name().toLowerCase() + "_sapling");
+		this.setCreativeTab(Archipelago.tab);
+		GameRegistry.registerBlock(this, tree.name().toLowerCase() + "_sapling");
+		Archipelago.PROXY.addItemRender(Item.getItemFromBlock(this), tree.name().toLowerCase() + "_sapling");
+		this.treeType = tree;
+	}
 
-    @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return SAPLING_AABB;
-    }
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return SAPLING_AABB;
+	}
 
-    @Override
-    public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
-        if (!world.isRemote) {
-            super.updateTick(world, pos, state, rand);
-            if (world.getLightFromNeighbors(pos.up()) >= 9 && rand.nextInt(7) == 0) {
-                this.grow(world, pos, state, rand);
-            }
-        }
-    }
+	@Override
+	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+		if (!world.isRemote) {
+			super.updateTick(world, pos, state, rand);
+			if (world.getLightFromNeighbors(pos.up()) >= 9 && rand.nextInt(7) == 0) {
+				this.grow(world, pos, state, rand);
+			}
+		}
+	}
 
-    public void grow(World world, BlockPos pos, IBlockState state, Random rand) {
-        if (state.getValue(STAGE) == 0) {
-            world.setBlockState(pos, state.cycleProperty(STAGE), 4);
-        } else {
-            this.generateTree(world, pos, rand);
-        }
-    }
+	public void grow(World world, BlockPos pos, IBlockState state, Random rand) {
+		if (state.getValue(STAGE) == 0) {
+			world.setBlockState(pos, state.cycleProperty(STAGE), 4);
+		} else {
+			this.generateTree(world, pos, rand);
+		}
+	}
 
-    public void generateTree(World world, BlockPos pos, Random rand) {
-        WorldGenerator gen = new WorldGenTrees(true);
+	public void generateTree(World world, BlockPos pos, Random rand) {
+		WorldGenerator gen = new WorldGenTrees(true);
 
-        if (!net.minecraftforge.event.terraingen.TerrainGen.saplingGrowTree(world, rand, pos))
-            return;
-        switch (treeType) {
-            case CORRIOSA:
-                gen = new WorldGenShrub(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.JUNGLE), EnumTrees.CORRIOSA.leaves.getDefaultState());
-                break;
-            case GALAPAGOS_MICONIA:
-                gen = new WorldGenShrub(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.JUNGLE), EnumTrees.GALAPAGOS_MICONIA.leaves.getDefaultState());
-                break;
-            case TABERNAEMONTANA_CERIFERA:
-                gen = new WorldGenShrub(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.JUNGLE), EnumTrees.TABERNAEMONTANA_CERIFERA.leaves.getDefaultState());
-                break;
-            default:
-                break;
-        }
-        gen.generate(world, rand, pos.down());
+		if (!net.minecraftforge.event.terraingen.TerrainGen.saplingGrowTree(world, rand, pos))
+			return;
+		switch (treeType) {
+		case KAPOK:
+			IBlockState iblockstate = EnumTrees.KAPOK.log.getDefaultState();
+			IBlockState iblockstate1 = EnumTrees.KAPOK.leaves.getDefaultState().withProperty(BlockLeaves.CHECK_DECAY, Boolean.valueOf(false));
+			gen = new WorldGenMegaJungle(true, 10, 20, iblockstate, iblockstate1);
+			break;
+		case CORRIOSA:
+			gen = new WorldGenShrub(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.JUNGLE), EnumTrees.CORRIOSA.leaves.getDefaultState().withProperty(BlockLeaves.CHECK_DECAY, Boolean.valueOf(false)));
+			break;
+		case GALAPAGOS_MICONIA:
+			gen = new WorldGenShrub(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.JUNGLE), EnumTrees.GALAPAGOS_MICONIA.leaves.getDefaultState().withProperty(BlockLeaves.CHECK_DECAY, Boolean.valueOf(false)));
+			break;
+		case TABERNAEMONTANA_CERIFERA:
+			gen = new WorldGenShrub(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.JUNGLE), EnumTrees.TABERNAEMONTANA_CERIFERA.leaves.getDefaultState().withProperty(BlockLeaves.CHECK_DECAY, Boolean.valueOf(false)));
+			break;
+		
+		default:
+			break;
+		}
+		gen.generate(world, rand, pos.down());
 
-    }
+	}
 
-    @Override
-    public boolean canGrow(World world, BlockPos pos, IBlockState state, boolean isClient) {
-        return true;
-    }
+	@Override
+	public boolean canGrow(World world, BlockPos pos, IBlockState state, boolean isClient) {
+		return true;
+	}
 
-    @Override
-    public boolean canUseBonemeal(World world, Random rand, BlockPos pos, IBlockState state) {
-        return (double) world.rand.nextFloat() < 0.45D;
-    }
+	@Override
+	public boolean canUseBonemeal(World world, Random rand, BlockPos pos, IBlockState state) {
+		return (double) world.rand.nextFloat() < 0.45D;
+	}
 
-    @Override
-    public void grow(World world, Random rand, BlockPos pos, IBlockState state) {
-        this.grow(world, pos, state, rand);
-    }
+	@Override
+	public void grow(World world, Random rand, BlockPos pos, IBlockState state) {
+		this.grow(world, pos, state, rand);
+	}
 
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(STAGE, meta);
-    }
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(STAGE, meta);
+	}
 
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(STAGE);
-    }
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(STAGE);
+	}
 
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, STAGE);
-    }
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, STAGE);
+	}
 }
