bf068326e1cf42de37f43cb057068e31804d610f
Alex-the-666 <alex.rowlands@cox.net>
2016-07-30 11:29:28 -0700

    bettr campfire

--- src/main/java/com/github/alexthe666/archipelago/block/BlockCampfire.java
+++ src/main/java/com/github/alexthe666/archipelago/block/BlockCampfire.java
@@ -9,17 +9,28 @@ import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
 public class BlockCampfire extends Block {
 
     public static final PropertyBool ACTIVE = PropertyBool.create("active");
+    protected static final AxisAlignedBB AABB = new AxisAlignedBB(0D, 0D, 0D, 1D, 0.25D, 1D);
 
     public BlockCampfire() {
         super(Material.WOOD);
@@ -48,11 +59,45 @@ public class BlockCampfire extends Block {
         return new BlockStateContainer(this, ACTIVE);
     }
 
-    public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ){
         if(worldIn.getBlockState(pos).getValue(ACTIVE)){
 
-        }else if(playerIn.getActiveItemStack() != null && playerIn.getActiveItemStack().getItem() != null && playerIn.getActiveItemStack().getItem() == Items.FLINT && new Random().nextInt(3) == 0){
+        }else if(heldItem != null && heldItem.getItem() != null && heldItem.getItem() == Items.FLINT){
             worldIn.setBlockState(pos, worldIn.getBlockState(pos).withProperty(ACTIVE, true));
         }
+        return true;
+    }
+
+    @Deprecated
+    public int getLightValue(IBlockState state) {
+        return this.lightValue = state.getValue(ACTIVE) == true ? 1 : 0;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public boolean shouldSideBeRendered(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+        return false;
+    }
+
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return AABB;
+    }
+
+    @Nullable
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, World worldIn, BlockPos pos) {
+        return AABB;
+    }
+
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT;
     }
 }
